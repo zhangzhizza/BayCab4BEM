e89df14e53c9d448928e3e1e61551d0b034e97b3
zhiangz <zhangzhizza@gmail.com>
2017-12-05 10:32:57 -0500

    mcmc out drawing function added.

--- src/loadPyStanRes.py
+++ src/loadPyStanRes.py
@@ -1,9 +1,11 @@
 import pickle as pk
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 import os
 import sys
+import csv
 
 def getTrace(resDir):
 	model = pk.load(open(resDir + os.sep + 'model.pkl', 'rb'));
@@ -14,10 +16,43 @@ def showPlot(resDir):
 	trace = getTrace(resDir);
 	theta = np.array(trace['theta']);
 	# Plot for each trace
-	sns.distplot(theta[:, i], 100, False, label = r'$\theta_%d$'%i) for i in range(theta.shape[0])
-	plt.xlabel('Normalized Calibration Parameter Value');
-	plt.ylabel('Distribution')
+	colorlist = ['red', 'green', 'blue', 'm', 'orange', 'y', 'sienna']
+	sns.set(font_scale=1.4) 
+	bins = 100;
+	for i in range(theta.shape[1]):
+		kde_kws = {'label': r'$\theta_%d KDE$'%i,
+			   'clip': (0.0, 1.0)};
+		hist_kws = {'label': r'$\theta_%d Histogram$'%i}
+		target = theta[:, i];
+		filtered = target[(target > 0) & (target < 1.0)];
+		if i < len(colorlist):
+			sns.distplot(filtered, bins = bins, color = colorlist[i], kde_kws = kde_kws, hist_kws = hist_kws);
+		else:
+			sns.distplot(filtered, bins = bins, kde_kws = kde_kws, hist_kws = hist_kws);
+	plt.xlabel('Normalized Calibration Parameter Value', fontsize = 20);
+	plt.ylabel('Density', fontsize = 20)
+	plt.ylim(ymax=5.0);
 	plt.show()
 
+def caluculateMode(resDir):
+	trace = getTrace(resDir);
+	theta = np.array(trace['theta']);
+	res = [];
+	# Calulate the mode for each trace
+	for i in range(theta.shape[1]):
+		this_trace = theta[:, i];
+		bins = 100;
+		this_hist = np.histogram(this_trace, bins = bins, range = (0,1))
+		this_mode = ((1/bins)/2.0) + (1/bins)* np.argmax(this_hist[0]);
+		res.append('theta_%d mode is %0.04f.'%(i, this_mode));
+	print (res)
+	with open("%s/mode.csv"%resDir,'w') as resultFile:
+		resultFile.write('\n'.join(res));
+		resultFile.close();
+
+
+
+
 if __name__ == "__main__":
+	caluculateMode(sys.argv[1])
 	showPlot(sys.argv[1])
